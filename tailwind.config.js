a5bbe98961aadf839374bb1ada4bce12320920e1
Asil <Turkmenasil@hotmail.com>
2024-07-27 23:41:13 +0300

    upgrade
    about section finished
    google reviews and location added
    some links added

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         gemunu:["Gemunu Libre", "sans-serif"],
         open:["Open Sans", "sans-serif"],
         oswald:["Oswald", 'sans-serif'],
+        chrimson:["Crimson Text", "serif"],
         
       },
 
@@ -23,6 +24,7 @@ module.exports = {
         'gega-melon': '#FFD369',
         'gega-grey': '#DDDDDD',
         'gega-white': '#F7F7F7',
+        'yayla-white':'#CACFD2',
       },
       spacing:{
         '128':'38.5rem'
