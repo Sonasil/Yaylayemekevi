1f44b5ca2fe3611a1ed8fdb008f5b90961963a91
Asil <Turkmenasil@hotmail.com>
2024-08-01 23:21:19 +0300

    menu
    menu page added
    some bugs fixed

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,26 @@
         var menu = document.getElementById('mobile-menu');
         menu.classList.toggle('hidden');
     });
+
+
+    document.getElementById('kahvaltı').addEventListener('click',function(){
+        var foodm = document.getElementById('mobile-kahvaltı');
+        foodm.classList.toggle('hidden');
+    });
+
+    
+    document.getElementById('ızgara').addEventListener('click',function(){
+        var foodm = document.getElementById('mobile-ızgara');
+        foodm.classList.toggle('hidden');
+    });
+
+    document.getElementById('corba').addEventListener('click',function(){
+        var foodm = document.getElementById('mobile-corba');
+        foodm.classList.toggle('hidden');
+    });
+
+    document.getElementById('icecek').addEventListener('click',function(){
+        var foodm = document.getElementById('mobile-icecek');
+        foodm.classList.toggle('hidden');
+    });
+
