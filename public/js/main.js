8680096536d4b43cd4ded73085a57edf4d2154b1
Asil <Turkmenasil@hotmail.com>
2024-08-05 17:19:53 +0300

    Finish
    Google reviews section bug fixed
    menu page finished

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,4 @@
     });
 
 
-    document.getElementById('kahvaltı').addEventListener('click',function(){
-        var foodm = document.getElementById('mobile-kahvaltı');
-        foodm.classList.toggle('hidden');
-    });
-
     
-    document.getElementById('ızgara').addEventListener('click',function(){
-        var foodm = document.getElementById('mobile-ızgara');
-        foodm.classList.toggle('hidden');
-    });
-
-    document.getElementById('corba').addEventListener('click',function(){
-        var foodm = document.getElementById('mobile-corba');
-        foodm.classList.toggle('hidden');
-    });
-
-    document.getElementById('icecek').addEventListener('click',function(){
-        var foodm = document.getElementById('mobile-icecek');
-        foodm.classList.toggle('hidden');
-    });
-
